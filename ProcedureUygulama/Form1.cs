027822afddfd695e1d702c02b1174be98635fc52
Emre GEMİCİ <gmcc.emree@gmail.com>
2022-03-31 02:38:25 +0300

    Add project files.

---
+++ ProcedureUygulama/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcedureUygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Server=. ; Database=Northwind ; Integrated Security=true");
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("UrunEkle", con);
+            // "UrunEkle" komutunun Store Procedure olduğunu belirttik.
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@urunAdi", txtUrunAdi.Text);
+            cmd.Parameters.AddWithValue("@birimFiyat", nudFiyat.Value);
+            cmd.Parameters.AddWithValue("@hedefStokDuzeyi", nudStok.Value);
+
+            con.Open();
+            try
+            {
+                int etk = cmd.ExecuteNonQuery();
+                con.Close();
+                if (etk > 0)
+                {
+                    MessageBox.Show("Kayıt Eklendi.");
+                    UrunListele();
+                }
+                else
+                {
+                    MessageBox.Show("Ürün Listede Mevcut.");
+                }
+            }
+            catch (Exception)
+            {
+                con.Close();
+                MessageBox.Show("Kayıt eklenirken hata oluştu.");
+            }
+        }
+
+        private void UrunListele()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("Select * from Urunler where Sonlandi=0", con);
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UrunListele();
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                SqlCommand cmd = new SqlCommand("UrunSil", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", dataGridView1.CurrentRow.Cells["UrunID"].Value);
+                con.Open();
+                try
+                {
+                    int etk = cmd.ExecuteNonQuery();
+                    con.Close();
+                    if (etk > 0)
+                    {
+                        MessageBox.Show($"{etk} row affected \n Kayıt Silindi.");
+                        UrunListele();
+                    }
+                    else
+                    {
+                        MessageBox.Show("(0) row affected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    con.Close();
+                    MessageBox.Show("Kayıt Silinirken Bir Hata Oluştu." + ex.Message);
+                }
+            }
+        }
+
+        private void btnKategoriListe_Click(object sender, EventArgs e)
+        {
+            KategoriForm kf = new KategoriForm();
+            kf.ShowDialog();
+        }
+    }
+}
---
+++ ProcedureUygulama/KategoriForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcedureUygulama
+{
+    public partial class KategoriForm : Form
+    {
+        public KategoriForm()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Server=. ; database = Northwind ; Integrated Security=true");
+        private void KategoriForm_Load(object sender, EventArgs e)
+        {
+            KategoriListele();
+        }
+
+        private void KategoriListele()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("KategoriListe", con);
+            adp.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+            DataTable dt = new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("KategoriEkle", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            if (txtKategoriAdi.Text == "" || txtTanimi.Text == "")
+            {
+                MessageBox.Show("Lüften boş alanları doldudurunuz.");
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue("@kategoriAdi", txtKategoriAdi.Text);
+                cmd.Parameters.AddWithValue("@tanimi", txtTanimi.Text);
+                con.Open();
+
+                try
+                {
+                    int etk = cmd.ExecuteNonQuery();
+                    con.Close();
+                    if (etk > 0)
+                    {
+                        MessageBox.Show($"{etk} row(s) affected \n Kategori Eklendi.");
+                        KategoriListele();
+                    }
+                    else
+                    {
+                        MessageBox.Show($"{etk} row(s) affected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    con.Close();
+                    MessageBox.Show("Kayıt eklenirken bir hata oluştu." + ex.Message);
+                }
+            }
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                SqlCommand cmd = new SqlCommand("KategoriSil", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", dataGridView1.CurrentRow.Cells["KategoriID"].Value);
+
+                con.Open();
+                try
+                {
+                    int etk = cmd.ExecuteNonQuery();
+                    con.Close();
+                    if (etk > 0)
+                    {
+                        MessageBox.Show($"{etk} row(s) affected \n Kategori Silindi.");
+                        KategoriListele();
+                    }
+                    else
+                    {
+                        MessageBox.Show($"{etk} row(s) affected");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    con.Close();
+                    MessageBox.Show("Kayıt silinirken bir hata oluştu." + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("KategoriGuncelle", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            cmd.Parameters.AddWithValue("@id", row.Cells["KategoriID"].Value);
+            cmd.Parameters.AddWithValue("@adi", row.Cells["KategoriAdi"].Value);
+            cmd.Parameters.AddWithValue("@tanimi", row.Cells["Tanimi"].Value);
+
+            con.Open();
+            try
+            {
+                int etk = cmd.ExecuteNonQuery();
+                con.Close();
+                if (etk > 0)
+                {
+                    MessageBox.Show($"{etk} row(s) affected \n Kategori Güncellendi.");
+                    KategoriListele();
+                }
+                else
+                {
+                    MessageBox.Show($"{etk} row(s) affected");
+                }
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                MessageBox.Show("Kayıt güncellenirken bir hata oluştu." + ex.Message);
+            }
+        }
+    }
+}
